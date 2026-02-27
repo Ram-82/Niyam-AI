9554727448f626a5503769d50b8b8b73bce08ec5
Ramachandragowda S Patil <121176737+Ram-82@users.noreply.github.com>
2026-02-27 13:59:21 +0530

    Restrict CORS to specific allowed origins
    Updated ALLOWED_ORIGINS to specify allowed domains.

--- niyam-backend/app/config.py
+++ niyam-backend/app/config.py
@@ -15,7 +15,8 @@ class Settings:
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 days
     
     # CORS Configuration
-    ALLOWED_ORIGINS: list = ["*"] # Allow all for development debugging
+    ALLOWED_ORIGINS: list = ["http://localhost:3000",
+    "https://niyam-ai-zncb.vercel.app"] # Allow all for development debugging
     
     # ALLOWED_ORIGINS: list = [
     #     "http://localhost:3000",
